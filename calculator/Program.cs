2cfc16ccc8101fe8cc8b5e093e75289f3f41b8a4
Samticode <samti8686@gmail.com>
2024-10-18 14:00:33 +0200

    first console application in c sharp

---
+++ calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int result;
+
+            string operation;
+
+            System.Console.WriteLine("Hello, welcome to the calculator program");
+
+            System.Console.WriteLine("Enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Enter the operation you want to perform, s for sub, m for mul, d for div, a for add");
+            operation = Console.ReadLine();
+
+            if (operation == "s")
+            {
+                result = num1 - num2;
+                System.Console.WriteLine("The result is " + result);
+            }
+            else if (operation == "m")
+            {
+                result = num1 * num2;
+                System.Console.WriteLine("The result is " + result);
+            }
+            else if (operation == "d")
+            {
+                result = num1 / num2;
+                System.Console.WriteLine("The result is " + result);
+            }
+            else if (operation == "a")
+            {
+                result = num1 + num2;
+                System.Console.WriteLine("The result is " + result);
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid operation");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ dicegames/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Dicegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int playerRandumNum;
+            int enemyRandumNum;
+
+            int playerScore = 0;
+            int enemyScore = 0;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine("Press any key to roll the dice");
+                playerRandumNum = random.Next(1, 7); 
+                enemyRandumNum = random.Next(1, 7);
+                
+                System.Console.ReadKey();
+                System.Console.WriteLine("You rolled: " + playerRandumNum);
+                System.Console.WriteLine("...");
+                System.Threading.Thread.Sleep(500);
+                System.Console.WriteLine("Enemy rolled: " + enemyRandumNum);
+
+                playerScore += playerRandumNum;
+                enemyScore += enemyRandumNum;
+
+
+                if (playerRandumNum > enemyRandumNum)
+                {
+                    System.Console.WriteLine("You win this round!");
+                }
+                else if (playerRandumNum < enemyRandumNum)
+                {
+                    System.Console.WriteLine("You lose this round!");
+                }
+                else
+                {
+                    System.Console.WriteLine("It's a draw for this round!");
+                }
+            }
+
+            System.Console.WriteLine("Your score: " + playerScore);
+            System.Console.WriteLine("Enemy score: " + enemyScore);
+
+            if (playerScore > enemyScore)
+            {
+                System.Console.WriteLine("You win the game!");
+            }
+            else if (playerScore < enemyScore)
+            {
+                System.Console.WriteLine("You lose the game!");
+            }
+            else
+            {
+                System.Console.WriteLine("It's a draw for the game!");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
