5e326c3614c5fe66f310cca13d40aa118287cd61
Samticode <samti8686@gmail.com>
2024-10-18 14:17:12 +0200

    its to easy man

---
+++ guess/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            int guess = 0;
+            int attempts = 0;
+
+            System.Console.WriteLine("Guess the number between 1 and 100");
+
+            while (guess != randomNumber)
+            {
+                System.Console.Write("Enter your guess: ");
+                guess = int.Parse(System.Console.ReadLine());
+                attempts++;
+
+                if (guess > randomNumber)
+                {
+                    System.Console.WriteLine("Too high!");
+                }
+                else if (guess < randomNumber)
+                {
+                    System.Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    System.Console.WriteLine("You guessed the number!");
+                    System.Console.WriteLine("It took you " + attempts + " attempts");
+                }
+            }
+        }
+    }
+}
