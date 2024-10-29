e62511cd16d7dc4f53dddf4e167bb2ceaa501ae5
Samticode <samti8686@gmail.com>
2024-10-29 14:49:04 +0100

    web api boilerplate

---
+++ PizzaStore/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Pizzaria API", Description = "We do the PIzza", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizzaria API v1"));
+};
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
